bb41703004acdc987e9681ede4c4a0308c771728
Pitipong-tnk <67319010031@tnk.ac.th>
2025-05-26 17:29:09 +0700

    pons & pun

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuButton = document.querySelector('.tree-keed')
     const navBlock = document.querySelector('.block-nav')
-    
+
     menuButton.addEventListener('click', () => {
-       if (navBlock.style.display === 'block') {
+        if (navBlock.style.display === 'block') {
             navBlock.style.display = 'none';
         } else {
             navBlock.style.display = 'block';
         }
-    })  
-    
+    })
+
     window.addEventListener('resize', () => {
-        if(window.innerWidth > 715) {
+        if (window.innerWidth > 715) {
             navBlock.style.display = 'none'
-        } 
+        }
     })
 
     const header = document.getElementById('header-con')
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', () => {
         const currentScroll = window.pageYOffset || document.documentElement.scrollTop
 
-        if(currentScroll > lastScrollTop) {
+        if (currentScroll > lastScrollTop) {
             header.style.top = '-100px'
             navBlock.style.display = 'none'
         } else {
@@ -31,4 +31,26 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     AOS.init();
+
+    async function loadeProduct() {
+        const response = await fetch('http://127.0.0.1:8000/product')
+        const data = await response.json();
+
+        const prodoctItem = document.getElementById('product-item')
+        data.forEach(item => {
+            const div = document.createElement("div");
+            div.classList.add('product')
+            div.setAttribute("data-aos", "fade-up");
+            div.innerHTML = `
+                <div class="image-product">
+                    <img src="data:image/jpeg;base64,${item.image}" alt="">
+                </div>
+                <div class="name-product">
+                    <span>${item['name']}</span>
+                </div>`
+            prodoctItem.appendChild(div)
+        });
+    }
+
+    loadeProduct()
 })

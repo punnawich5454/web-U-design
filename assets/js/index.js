f98e570fee79985be9f5e7dcdb88dc36b3507e79
Pitipong-tnk <67319010031@tnk.ac.th>
2025-05-27 17:27:10 +0700

    pons & pun

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,25 +32,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
     AOS.init();
 
-    async function loadeProduct() {
-        const response = await fetch('http://127.0.0.1:8000/product')
-        const data = await response.json();
+    async function loadProduct() {
+        try {
+            const response = await fetch('http://127.0.0.1:3000/api/product')
+            const data = await response.json()
 
-        const prodoctItem = document.getElementById('product-item')
-        data.forEach(item => {
-            const div = document.createElement("div");
-            div.classList.add('product')
-            div.setAttribute("data-aos", "fade-up");
-            div.innerHTML = `
-                <div class="image-product">
-                    <img src="data:image/jpeg;base64,${item.image}" alt="">
-                </div>
-                <div class="name-product">
-                    <span>${item['name']}</span>
-                </div>`
-            prodoctItem.appendChild(div)
-        });
+            const productItem = document.getElementById('product-item')
+            data.forEach(item => {
+                const div = document.createElement('div')
+                div.classList.add('product')
+                div.setAttribute('data-aos', 'fade-up')
+                const html = `
+                    <div class="image-product">
+                        <img src="data:image/jpeg;base64,${item.image}" alt="">
+                    </div>
+                    <div class="name-product">
+                        <span>${item.name}</span>
+                    </div>`
+                div.innerHTML += html
+                productItem.appendChild(div)
+            });
+        } catch (err) {
+            console.error(err)
+        }
     }
 
-    loadeProduct()
+    function search() {
+        const keyword = document.getElementById('search-input').value
+        const resultFrame = document.getElementById('iframe-search')
+
+        const result = searchInput.value.trim()
+        if (result === "") {
+            resultFrame.src = `/components/search.html`
+        } else {
+            resultFrame.src = `/components/search.html?query=` + encodeURIComponent(keyword)    
+        }
+    }
+
+    const searchInput = document.getElementById('search-input')
+    if (searchInput) {
+        searchInput.addEventListener('input', () => {
+            search()
+        })
+    }
+
+    document.getElementById('button-1').addEventListener('click', (event) => {
+        event.preventDefault()
+        search()
+    })
+
+    loadProduct()
 })
+

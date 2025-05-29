c3992355ce913f65c4c9b099277a54486dc015d4
Pitipong-tnk <67319010031@tnk.ac.th>
2025-05-29 17:13:51 +0700

    pons & pun

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuButton = document.querySelector('.tree-keed')
     const navBlock = document.querySelector('.block-nav')
+    const searchInput = document.getElementById('search-input')
+    const resultFrame = document.getElementById('iframe-search')
 
     menuButton.addEventListener('click', () => {
         if (navBlock.style.display === 'block') {
@@ -24,6 +26,8 @@ document.addEventListener('DOMContentLoaded', () => {
         if (currentScroll > lastScrollTop) {
             header.style.top = '-100px'
             navBlock.style.display = 'none'
+            resultFrame.style.display = 'none'
+            searchInput.blur()
         } else {
             header.style.top = '0px'
         }
@@ -37,14 +41,16 @@ document.addEventListener('DOMContentLoaded', () => {
             const response = await fetch('http://127.0.0.1:3000/api/product')
             const data = await response.json()
 
+            const dataLimit = data.slice(0, 10)
             const productItem = document.getElementById('product-item')
-            data.forEach(item => {
+            productItem.innerHTML = ""
+            dataLimit.forEach(item => {
                 const div = document.createElement('div')
                 div.classList.add('product')
                 div.setAttribute('data-aos', 'fade-up')
                 const html = `
                     <div class="image-product">
-                        <img src="data:image/jpeg;base64,${item.image}" alt="">
+                        <img src="data:image/jpeg;base64,${item.image}" alt="img-product">
                     </div>
                     <div class="name-product">
                         <span>${item.name}</span>
@@ -58,22 +64,30 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function search() {
-        const keyword = document.getElementById('search-input').value
-        const resultFrame = document.getElementById('iframe-search')
+        const keyword = searchInput.value
 
         const result = searchInput.value.trim()
         if (result === "") {
             resultFrame.src = `/components/search.html`
+
         } else {
-            resultFrame.src = `/components/search.html?query=` + encodeURIComponent(keyword)    
+            resultFrame.src = `/components/search.html?query=` + encodeURIComponent(keyword)
+            resultFrame.style.display = 'block'
         }
     }
 
-    const searchInput = document.getElementById('search-input')
     if (searchInput) {
         searchInput.addEventListener('input', () => {
             search()
         })
+
+        // searchInput.addEventListener('focus', () => {
+        //     resultFrame.style.width = '70%'
+        // })
+
+        // searchInput.addEventListener('blur', () => {
+        //     resultFrame.style.width = '0%'
+        // })
     }
 
     document.getElementById('button-1').addEventListener('click', (event) => {
